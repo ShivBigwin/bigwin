ab5cfd980b575e9e34b23c3662322f57e15544f9
ShivBigwin <shivendra@bigwininfotech.in>
2025-07-23 11:23:49 +0530

    comment dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,25 @@
-"use client";
+// "use client";
 
-import Dashboard from '@/components/dashboard/index';
-import Loader from '@/components/loader/loader';
+// import Dashboard from '@/components/dashboard/index';
+// import Loader from '@/components/loader/loader';
 
-import React, { Suspense, useState } from 'react'
+// import React, { Suspense, useState } from 'react'
 
-const page = () => {
-const [isLoading, setIsLoading] = useState(true);
+// const page = () => {
+// const [isLoading, setIsLoading] = useState(true);
 
-  // Simulate loading
-  setTimeout(() => setIsLoading(false), 2000);
-  return (
-    <div>
-      {isLoading && <Loader />}
-    {/* <Navbar> */}
-    <Suspense fallback={<div>Loading...</div>}>
-        <Dashboard/>
-    </Suspense>
-    {/* </Navbar> */}
-    </div>
-  )
-}
+//   // Simulate loading
+//   setTimeout(() => setIsLoading(false), 2000);
+//   return (
+//     <div>
+//       {isLoading && <Loader />}
+//     {/* <Navbar> */}
+//     <Suspense fallback={<div>Loading...</div>}>
+//         <Dashboard/>
+//     </Suspense>
+//     {/* </Navbar> */}
+//     </div>
+//   )
+// }
 
-export default page
+// export default page
